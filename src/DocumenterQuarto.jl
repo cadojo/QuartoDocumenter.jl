351a1aeb3cce33acad503a1622c2598489381450
Joey Carpinelli <joey@loopy.codes>
2025-01-03 23:26:57 -0500

    Adds package UUID to generate

--- src/DocumenterQuarto.jl
+++ src/DocumenterQuarto.jl
@@ -30,9 +30,12 @@ Generate a documentation site from a default template.
 function generate(; title=nothing, type="book", api="api")
 
     name::Union{String,Nothing} = nothing
+    uuid::Union{String,Nothing} = nothing
     if isnothing(title)
         if isfile("Project.toml")
-            name = TOML.parsefile("Project.toml")["name"]
+            project = TOML.parsefile("Project.toml")
+            name = project["name"]
+            uuid = project["uuid"]
         end
 
         title = isnothing(name) ? "Documentation" : name
@@ -162,6 +165,7 @@ function generate(; title=nothing, type="book", api="api")
             Quarto = "d7167be5-f61b-4dc9-b75c-ab62374668c5"
             DocumenterQuarto = "73f83fcb-c367-40db-89b6-8fd94701aaf2"
             IJulia = "7073ff75-c697-5162-941a-fcdaad2a7d2a"
+            $name = "$uuid"
             """
         )
     end
